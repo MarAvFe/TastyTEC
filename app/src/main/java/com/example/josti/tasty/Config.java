b1e461cb5a3ab4057201113bf7f9d0b2c00729c8
maravfe <marcelloavila@gmail.com>
2016-04-11 09:49:01 -0600

    Tasty v1 Base

---
+++ app/src/main/java/com/example/josti/tasty/Config.java
@@ -0,0 +1,11 @@
+package com.example.josti.tasty;
+
+public final class Config {
+    private Config(){
+
+    }
+    public static final String getAPIkey = "AIzaSyALtaKPPsbXyciSzKf_mGYNVr9DuQNXl1k";
+    //API KEY FOR YOUTUBE API
+
+
+}
---
+++ app/src/main/java/com/example/josti/tasty/Main2Activity.java
@@ -0,0 +1,44 @@
+package com.example.josti.tasty;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Main2Activity extends YouTubeBaseActivity{
+
+    private YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.view_player);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("2eXCKFMzg2w");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        youTubePlayerView.initialize(Config.getAPIkey,onInitializedListener);
+
+
+    }
+
+}
