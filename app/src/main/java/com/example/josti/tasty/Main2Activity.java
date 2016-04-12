7ad8720f5e893b4914a49ef269fadc6942043bfa
Jostin Marín <Jostin Marn>
2016-04-11 21:14:49 -0600

    Usar clase de marcello

--- app/src/main/java/com/example/josti/tasty/Main2Activity.java
+++ app/src/main/java/com/example/josti/tasty/Main2Activity.java
@@ -6,20 +6,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
+import java.util.ArrayList;
+
 public class Main2Activity extends YouTubeBaseActivity{
 
     private YouTubePlayerView youTubePlayerView;
     private YouTubePlayer.OnInitializedListener onInitializedListener;
     String compartirUrl;
+    String listIngredients;
+    String listSteps;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +34,57 @@ public class Main2Activity extends YouTubeBaseActivity{
         //setSupportActionBar(toolbar);
         compartirUrl="http://www.youtube.com";
 
+
+        ArrayList pasos = new ArrayList<String>();
+        ArrayList ingredientes = new ArrayList<String>();
+
+
+
+        pasos.add("paso1");
+        pasos.add("paso2");
+        pasos.add("paso3");
+        pasos.add("paso4");
+        ingredientes.add("ing1");
+        ingredientes.add("ing2");
+        ingredientes.add("ing3");
+        ingredientes.add("ing4");
+        ingredientes.add("ing1");
+        ingredientes.add("ing2");
+        ingredientes.add("ing3");
+        ingredientes.add("ing4");
+        ingredientes.add("ing1");
+        ingredientes.add("ing2");
+        ingredientes.add("ing3");
+        ingredientes.add("ing4");
+        ingredientes.add("ing1");
+        ingredientes.add("ing2");
+        ingredientes.add("ing3");
+        ingredientes.add("ing4");
+
+     RecipeForShow receta = new RecipeForShow("Nombre de la receta", "descripcion de la receta", "23", "0M9cypF0Pyk", pasos, ingredientes);
+
+        listIngredients = retornarIngredientes(ingredientes);
+        listSteps = retornarPasos(pasos);
+
+        TextView tituloReceta = (TextView)findViewById(R.id.tituloReceta);
         ImageButton btnCompartir;
+
+        tituloReceta.setText(receta.getName());
         btnCompartir = (ImageButton)findViewById(R.id.btnCompartir);
+
+       TextView tvIngredients;
+        tvIngredients = (TextView)findViewById(R.id.ingredientes);
+        tvIngredients.setText(listIngredients);
+
+        TextView tvSteps;
+        tvSteps = (TextView)findViewById(R.id.preparation);
+        tvSteps.setText(listSteps);
+
+
+
         btnCompartir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent i = new Intent(Intent.ACTION_SEND);
                 i.setType("text/plain");
                 i.putExtra(Intent.EXTRA_SUBJECT, "Sharing URL");
@@ -61,4 +112,19 @@ public class Main2Activity extends YouTubeBaseActivity{
 
     }
 
+    public String retornarIngredientes(ArrayList<String> arreglo) {
+        String res="";
+        for(String s : arreglo){
+            res+="-"+s+"\n";
+        }
+        return res;
+    }
+
+    public String retornarPasos(ArrayList<String> arreglo) {
+        String res="";
+        for(String s : arreglo){
+            res+=s+"\n\n";
+        }
+        return res;
+    }
 }
